733ddc470a047f067c4a14ceaacb0aa08dc992a8
Nithiyanantham S A <96058658+NithiAnbu97@users.noreply.github.com>
2022-01-23 10:56:30 +0530

    Add files via upload

---
+++ Task-21/call_back.js
@@ -0,0 +1,149 @@
+var hid = document.createElement("div");
+hid.style.height = "50rem";
+hid.style.width = "50rem";
+hid.style.color = "Blue";
+hid.style.textAlign = "center"
+
+setTimeout(() =>{
+    hid.innerText ="Lets's Begin"
+}, 1000);
+
+setTimeout(() =>{
+    hid.innerText ="10"
+}, 2000);
+
+setTimeout(() =>{
+    hid.innerText ="9"
+}, 3000);
+
+setTimeout(() =>{
+    hid.innerText ="8"
+}, 4000);
+
+setTimeout(() =>{
+    hid.innerText ="7"
+}, 5000);
+
+setTimeout(() =>{
+    hid.innerText ="6"
+}, 6000);
+
+setTimeout(() =>{
+    hid.innerText ="5"
+}, 7000);
+
+setTimeout(() =>{
+    hid.innerText ="4"
+}, 8000);
+
+setTimeout(() =>{
+    hid.innerText ="3"
+}, 9000);
+
+setTimeout(() =>{
+    hid.innerText ="2"
+}, 10_000);
+
+setTimeout(() =>{
+    hid.innerText ="1"
+}, 11_000);
+
+setTimeout(() =>{
+    hid.innerText ="Happy Independence Day"
+}, 12_000);
+
+document.body.appendChild(hid);
+
+
+
+
+
+
+
+
+
+
+//   for try
+
+
+// const timeH = document.querySelector("h1");
+// let timeSecond = 10;
+
+// displayTime(timeSecond)
+
+// const countDown = setInterval (() =>{
+//     timeSecond--;
+//     displayTime(timeSecond);
+//     if(timeSecond <=0 || timeSecond < 1){
+//         endTime();
+//         clearInterval(countDown);
+//     }
+// }, 1000);
+ 
+// function displayTime(second) {
+//     const min = Math.floor(second / 60);
+//     const sec = Math.floor(second % 60);
+//     timeH.innerHTML = `${min<10 ? "0" : ""}${min}:${sec<10 ? "0" : ""}${sec} `
+    
+// }
+
+// function endTime(){
+//     timeH.innerHTML = "Happy Independence Day"
+// }
+
+
+
+
+// var i = 11;
+//  setTimeout(function(){
+//     document.getElementById('res').innerHTML="Let's Begin";
+
+//     setTimeout(function(){
+//         i--;
+//         document.getElementById('res').innerHTML=i;
+
+//        setTimeout(function(){
+//             i--;
+//             document.getElementById('res').innerHTML=i;
+    
+//             setTimeout(function(){
+//                 i--;
+//                 document.getElementById('res').innerHTML=i;
+//                setTimeout(function(){
+//                     i--;
+//                     document.getElementById('res').innerHTML=i;
+//                     setTimeout(function(){
+//                         i--;
+//                         document.getElementById('res').innerHTML=i;
+//                         setTimeout(function(){
+//                             i--;
+//                             document.getElementById('res').innerHTML=i;
+//                             setTimeout(function(){
+//                                 i--;
+//                                 document.getElementById('res').innerHTML=i;
+//                                 setTimeout(function(){
+//                                     i--;
+//                                     document.getElementById('res').innerHTML=i;
+//                                     setTimeout(function(){
+//                                         i--;
+//                                         document.getElementById('res').innerHTML=i;
+//                                         setTimeout(function(){
+//                                             i--;
+//                                             document.getElementById('res').innerHTML=i;
+//                                             setTimeout(function(){
+//                                               document.getElementById('res').innerHTML="Happy Independence Day";
+                                        
+//                                             },1500)
+//                                         },1500)    
+//                                     },1500)    
+//                                 },1500)
+//                             },1500)    
+//                         },1500)   
+//                     },1500)
+//                 },1500)
+//             },1500)
+//         },1500)
+//     },1500)
+// },1500)
+
+
